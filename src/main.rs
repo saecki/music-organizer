72a4c9a8a257579ec9e8d51ce905ab0e9abfef37
Saecki <tobiasschmitz2001@gmail.com>
2020-09-04 17:10:43 +0200

    impoved cli

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,17 @@ pub struct Metadata {
     pub title: String,
 }
 
+#[derive(Default, Debug, PartialEq)]
+pub struct FileMove {
+    old: PathBuf,
+    new: PathBuf,
+}
+
+#[derive(Default, Debug, PartialEq)]
+pub struct DirCreation {
+    path: PathBuf,
+}
+
 impl Metadata {
     pub fn read_from(path: &PathBuf) -> Self {
         match path.extension().unwrap().to_str().unwrap() {
@@ -153,9 +164,17 @@ fn main() {
     let yes = matches.is_present("assume-yes");
     let verbose = matches.is_present("verbose");
 
+    let abs_music_dir = match PathBuf::from(&music_dir).canonicalize() {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Not a valid music dir path: {}\n{:?}", music_dir.display(), e);
+            exit(1)
+        }
+    };
+
     let output_dir = match matches.value_of("output-dir") {
         Some(s) => PathBuf::from(s),
-        None => music_dir.clone(),
+        None => abs_music_dir.clone(),
     };
 
     if !output_dir.exists() {
@@ -165,13 +184,6 @@ fn main() {
         }
     }
 
-    let abs_music_dir = match PathBuf::from(&music_dir).canonicalize() {
-        Ok(t) => t,
-        Err(e) => {
-            println!("Not a valid music dir path: {}\n{:?}", music_dir.display(), e);
-            exit(1)
-        }
-    };
 
     println!("indexing...");
     let mut artists = Vec::new();
@@ -251,12 +263,12 @@ fn main() {
         });
     }
 
-    println!("\nchecking songs");
+    println!("\nchecking...");
 
     let mut offset = 1;
     for ar1 in artists.iter() {
         for ar2 in artists.iter().skip(offset) {
-            if i != j && ar1.name.eq_ignore_ascii_case(&ar2.name) {
+            if ar1.name.eq_ignore_ascii_case(&ar2.name) {
                 println!("These two artists are named similarly:\n{}\n{}", &ar1.name, &ar2.name);
                 let index = input_options_loop(&[
                     "don't do anything",
@@ -295,46 +307,30 @@ fn main() {
         offset += 1;
     }
 
-    if !yes {
-        let ok = input_confirmation_loop(&format!(
-            "{} files will be {}. Continue",
-            songs.len(),
-            if copy { "copied" } else { "moved" })
-        );
+    let mut dir_creations = Vec::new();
+    let mut file_moves = Vec::with_capacity(songs.len() / 10);
 
-        if !ok {
-            println!("exiting...");
-            exit(1);
-        }
-    }
-
-    unsafe {
-        LAST_LEN = 0;
-    }
-
-    println!("\nwriting...");
-    let mut counter: usize = 1;
     for ar in &artists {
         let ar_dir = output_dir.join(valid_os_string(&ar.name));
         if !ar_dir.exists() {
-            if let Err(e) = std::fs::create_dir(&ar_dir) {
-                println!("error creating dir: {}:\n{}", ar_dir.display(), e);
-            }
+            dir_creations.push(DirCreation { path: ar_dir.clone() });
         }
 
         for al in &ar.albums {
+            let single_album_name = format!("{} - single", songs[al.songs[0]].title.to_lowercase());
+            let is_single = al.name.is_empty() || al.songs.len() == 1 && al.name.to_lowercase() == single_album_name;
             let al_dir = ar_dir.join(valid_os_string(&al.name));
-            if !al_dir.exists() {
-                if let Err(e) = std::fs::create_dir(&al_dir) {
-                    println!("error creating dir: {}:\n{}", al_dir.display(), e);
-                }
+
+            if !is_single && !al_dir.exists() {
+                dir_creations.push(DirCreation { path: al_dir.clone() });
             }
 
             for si in &al.songs {
                 let song = &songs[*si];
                 let extension = song.current_file.extension().unwrap();
 
-                if al.name.is_empty() || al.name.to_ascii_lowercase() == format!("{} - single", &song.title.to_ascii_lowercase()) {
+                let new_file;
+                if is_single {
                     let mut file_name = OsString::with_capacity(4 + song.artist.len() + song.title.len() + extension.len());
 
                     file_name.push(valid_os_string(&song.artist));
@@ -343,9 +339,7 @@ fn main() {
                     file_name.push(".");
                     file_name.push(extension);
 
-                    let new_file = ar_dir.join(file_name);
-
-                    mv_or_cp(&counter, &song.current_file, &new_file, copy, verbose);
+                    new_file = ar_dir.join(file_name);
                 } else {
                     let mut file_name = OsString::with_capacity(9 + song.artist.len() + song.title.len() + extension.len());
 
@@ -356,11 +350,15 @@ fn main() {
                     file_name.push(".");
                     file_name.push(extension);
 
-                    let new_file = al_dir.join(file_name);
-
-                    mv_or_cp(&counter, &song.current_file, &new_file, copy, verbose);
+                    new_file = al_dir.join(file_name);
                 }
-                counter += 1;
+
+                if new_file != song.current_file {
+                    file_moves.push(FileMove {
+                        old: song.current_file.clone(),
+                        new: new_file,
+                    });
+                }
             }
         }
     }
@@ -368,18 +366,74 @@ fn main() {
     if !unknown.is_empty() {
         let unknown_dir = output_dir.join("unknown");
         if !unknown_dir.exists() {
-            if let Err(e) = std::fs::create_dir(&unknown_dir) {
-                println!("Error creating dir: {}:\n{}", unknown_dir.display(), e);
-            }
+            dir_creations.push(DirCreation { path: unknown_dir.clone() });
         }
         for si in &unknown {
             let song = &songs[*si];
             let new_file = unknown_dir.join(song.current_file.file_name().unwrap());
 
-            mv_or_cp(&counter, &song.current_file, &new_file, copy, verbose);
-            counter += 1;
+            file_moves.push(FileMove {
+                old: song.current_file.clone(),
+                new: new_file,
+            });
         }
-        println!();
+    }
+    println!();
+
+    if dir_creations.is_empty() && file_moves.is_empty() {
+        println!("noting to do exiting...");
+        exit(0);
+    }
+
+    if !yes {
+        if verbose {
+            if !dir_creations.is_empty() {
+                println!("dirs:");
+                for (i, d) in dir_creations.iter().enumerate() {
+                    println!("{} {}", i + 1, d.path.display());
+                }
+                println!();
+            }
+            if !file_moves.is_empty() {
+                println!("files:");
+                for (i, f) in file_moves.iter().enumerate() {
+                    println!("{} {}", i + 1, f.new.display());
+                }
+                println!();
+            }
+        }
+
+        let ok = input_confirmation_loop(&format!(
+            "{} dirs will be created.\n{} files will be {}. Continue",
+            dir_creations.len(),
+            file_moves.len(),
+            if copy { "copied" } else { "moved" })
+        );
+
+        if !ok {
+            println!("exiting...");
+            exit(1);
+        }
+    }
+
+    println!("\nwriting...");
+
+    unsafe {
+        LAST_LEN = 0;
+    }
+    for (i, d) in dir_creations.iter().enumerate() {
+        match std::fs::create_dir(&d.path) {
+            Ok(_) => print_verbose(&format!("{} creating dir {}", i, d.path.display()), verbose),
+            Err(e) => println!("error creating dir: {}:\n{}", d.path.display(), e),
+        }
+    }
+    println!();
+
+    unsafe {
+        LAST_LEN = 0;
+    }
+    for (i, f) in file_moves.iter().enumerate() {
+        mv_or_cp(&(i + 1), &f.old, &f.new, copy, verbose);
     }
 
     println!("\ndone")
@@ -397,16 +451,14 @@ fn is_music_extension(s: &OsStr) -> bool {
 }
 
 fn mv_or_cp(song_index: &usize, old: &PathBuf, new: &PathBuf, copy: bool, verbose: bool) {
-    if old == new {
-        print_verbose(&format!("skipping {} {}", song_index, new.display()), verbose);
-    } else if copy {
-        print_verbose(&format!("copying {} {}", song_index, new.display()), verbose);
+    if copy {
+        print_verbose(&format!("{} copying {}", song_index, new.display()), verbose);
         let _ = std::io::stdout().flush().is_ok();
         if let Err(e) = std::fs::copy(old, new) {
             println!("\nerror: {}", e);
         }
     } else {
-        print_verbose(&format!("moving {} {}", song_index, new.display()), verbose);
+        print_verbose(&format!("{} moving {}", song_index, new.display()), verbose);
         let _ = std::io::stdout().flush().is_ok();
         if let Err(e) = std::fs::rename(old, new) {
             println!("\nerror: {}", e);
