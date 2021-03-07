9a0c269e14ea11b7877354f1e4c40b359c041b5c
Saecki <tobiasschmitz2001@gmail.com>
2021-03-07 15:53:12 +0100

    fix completion file names

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ sudo cp target/release/cli /usr/local/bin/music_organizer
 case "$SHELL" in
     *bash)
 	echo "creating a completion script for bash"
-	/usr/local/bin/music_organizer -g "bash" | sudo tee /etc/bash_completion.d/_music_organizer > /dev/null
+	/usr/local/bin/music_organizer -g "bash" | sudo tee /etc/bash_completion.d/music_organizer > /dev/null
 	;;
     *zsh)
 	echo "creating a completion script for zsh"
-	/usr/local/bin/music_organizer -g "zsh" | sudo tee /usr/share/zsh/site-functions/music_organizer > /dev/null
+	/usr/local/bin/music_organizer -g "zsh" | sudo tee /usr/share/zsh/site-functions/_music_organizer > /dev/null
 	;;
     *)
 	echo "create a completion script for your shell manually by running 'music_organizer --generate-completion <shell>'"
