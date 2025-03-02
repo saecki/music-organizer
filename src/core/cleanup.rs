6064eac6e8913c3d0fd78fd581fa137bf89df4a4
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-03-03 00:04:02 +0100

    fix: check sub-dirs even if parent-dir is non-empty

--- src/core/cleanup.rs
+++ src/core/cleanup.rs
@@ -2,25 +2,48 @@ use std::path::{Path, PathBuf};
 
 use crate::fs::DirDeletion;
 
-fn is_empty_dir(cleanup: &mut Cleanup, dir: &Path, f: &mut impl FnMut(&Path)) -> bool {
-    if dir.is_file() {
-        return false;
-    };
+fn check_empty_dir(
+    cleanup: &mut Cleanup,
+    dir_stack: &mut Vec<(PathBuf, std::fs::ReadDir, bool)>,
+    f: &mut impl FnMut(&Path),
+) {
+    // traverse depth first to be more memory efficient
+    'stack: while let Some((_, dir_iter, is_empty)) = dir_stack.last_mut() {
+        while let Some(entry) = dir_iter.next() {
+            let Ok(entry) = entry else {
+                *is_empty = false;
+                continue;
+            };
 
-    f(dir);
-
-    if let Ok(r) = std::fs::read_dir(dir) {
-        for e in r.into_iter().filter_map(|e| e.ok()) {
-            if !is_empty_dir(cleanup, &e.path(), f) {
-                return false;
+            let Ok(ft) = entry.file_type() else { continue };
+            if ft.is_file() {
+                *is_empty = false;
+                continue;
             }
+
+            let dir_path = entry.path();
+            f(&dir_path);
+
+            let Ok(dir_iter) = std::fs::read_dir(&dir_path) else {
+                *is_empty = false;
+                continue;
+            };
+
+            // traverse sub-directory before continuing here
+            dir_stack.push((dir_path, dir_iter, true));
+            continue 'stack;
         }
 
-        cleanup.dir_deletions.push(DirDeletion { path: dir.to_owned() });
-        return true;
+        let (dir_path, _, is_empty) = dir_stack.pop().unwrap();
+        if is_empty {
+            cleanup.dir_deletions.push(DirDeletion { path: dir_path });
+        } else {
+            // mark parent directory as non-empty if this directory is non-empty
+            if let Some((_, _, parent_is_empty)) = dir_stack.last_mut() {
+                *parent_is_empty = false;
+            }
+        }
     }
-
-    false
 }
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
@@ -37,12 +60,11 @@ impl From<PathBuf> for Cleanup {
 
 impl Cleanup {
     pub fn check(&mut self, f: &mut impl FnMut(&Path)) {
-        let dir = self.music_dir.to_owned();
+        let dir_path = self.music_dir.to_owned();
 
-        if let Ok(r) = std::fs::read_dir(dir) {
-            for e in r.into_iter().filter_map(|e| e.ok()) {
-                is_empty_dir(self, &e.path(), f);
-            }
+        if let Ok(dir_iter) = std::fs::read_dir(&dir_path) {
+            let mut dir_stack = vec![(dir_path, dir_iter, true)];
+            check_empty_dir(self, &mut dir_stack, f);
         }
     }
 
